5a1a90e5c02147e5712503986af0c8f5dc924663
Eric Pait <ericpait@gmail.com>
2015-01-05 17:18:00 -0500

    Setup gulp to compile scss and reload with browsersync

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+
+
+// SASS Task
+gulp.task('sass', function () {
+    gulp.src(['./scss/**/*.scss'])
+        .pipe(plugins.rubySass({sourcemapPath: '../scss', compass: true}))
+        .on('error', function (err) { console.log(err.message); })
+        .pipe(gulp.dest('./css'))
+        .pipe(plugins.filter('**/*.css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// Default gulp task with Browser Sync
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch('**/*.scss', ['sass']);
+});
